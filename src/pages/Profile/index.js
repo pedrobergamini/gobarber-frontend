6128dace9857b2595f1fa34b3a60f6f8de7478b0
Pedro Bergamini <pedrobergamini@MacBook-Pro-de-Pedro.local>
2019-10-12 14:43:55 -0300

    private repo

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+export default function Profile() {
+  const { profile } = useSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id"/>
+
+        <Input name="name" placeholder="Seu nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Sua nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirme sua senha"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
+}
